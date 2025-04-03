a78007f6dba5e14d0802d5c554091d3ad1417d2d
natha <108113193+Mandaiya@users.noreply.github.com>
2025-04-03 09:00:24 +0530

    Update thumbnails.py

--- src/modules/utils/thumbnails.py
+++ src/modules/utils/thumbnails.py
@@ -96,7 +96,7 @@ def add_controls(img: Image.Image) -> Image.Image:
 
     mask = Image.new("L", dark_region.size, 0)
     ImageDraw.Draw(mask).rounded_rectangle(
-        (150, 150, box[2] - box[0], box[3] - box[1]), 150, fill=255
+        (0, 0, box[2] - box[0], box[3] - box[1]), 0, fill=255
     )
 
     img.paste(dark_region, box, mask)
